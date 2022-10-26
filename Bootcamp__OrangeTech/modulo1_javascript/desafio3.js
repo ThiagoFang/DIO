c19f1d6815d0b9e17fde1995df0e8e5aaa5c99b3
Thiago <thiagofang@gmail.com>
2022-10-26 20:16:52 -0300

    bootcamp orangetech: resposta para o terceiro desafio

--- Bootcamp__OrangeTech/modulo1_javascript/desafio3.js
+++ Bootcamp__OrangeTech/modulo1_javascript/desafio3.js
@@ -12,3 +12,15 @@
 //   - Entre 25 e 30, Acima do peso;
 //   - Entre 30 e 40, Obeso;
 //   - Acima de 40, Obesidade Grave;
+
+const calcularIMC = (peso, altura) => {
+  const IMC = (peso / (altura * altura)).toFixed(2);
+
+  if(IMC < 18.5) return `O Seu IMC é ${IMC}, você está abaixo do peso!`;
+  if(IMC >= 18.5 && IMC < 25) return `O Seu IMC é ${IMC}, Seu peso esta normal!`;
+  if(IMC >= 25 && IMC < 30) return `O Seu IMC é ${IMC}, Você está acima do peso!`;
+  if(IMC >= 30 && IMC < 40) return `O Seu IMC é ${IMC}, Você esta com Obesidade!`;
+  if(IMC >=  40) return `O Seu IMC é ${IMC}, Você esta com Obesidade grave!`;
+};
+
+console.log(calcularIMC(80, 1.60))
