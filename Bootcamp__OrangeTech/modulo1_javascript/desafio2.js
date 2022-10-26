3164315922396c56ca7beb9d29cfdcd1669aa95e
Thiago <thiagofang@gmail.com>
2022-10-26 20:12:17 -0300

    bootcamp orangetech: preparando setup do terceiro desafio

--- Bootcamp__OrangeTech/modulo1_javascript/desafio2.js
+++ Bootcamp__OrangeTech/modulo1_javascript/desafio2.js
@@ -1,4 +1,4 @@
-// 1 -  Faça um algorítimo que dado as 3 notas tiradas por um aluno em um semestre da faculdade, 
+// 2 -  Faça um algorítimo que dado as 3 notas tiradas por um aluno em um semestre da faculdade, 
 // calcule e imprima a sua média e a sua classificação conforme a tabela abaixo
 
 // Média = (nota 1 + nota 2 + nota 3) / 3;
