cd5a1fe204f4f9f634754a605c312b0b774e6f6d
Thiago <thiagofang@gmail.com>
2022-10-26 16:54:41 -0300

    bootcamp orangetech: responsta do segundo desafio

---
+++ Bootcamp__OrangeTech/modulo1_javascript/desafio2.js
@@ -0,0 +1,19 @@
+// 1 -  Faça um algorítimo que dado as 3 notas tiradas por um aluno em um semestre da faculdade, 
+// calcule e imprima a sua média e a sua classificação conforme a tabela abaixo
+
+// Média = (nota 1 + nota 2 + nota 3) / 3;
+
+// Classificação:
+//   - Média menor que 5, reprovação;
+//   - Média entre 5 e 7, recuperação;
+//   - Média maior de 7, passou de semestre;
+
+const calcularMédia = (nota1, nota2, nota3) => {
+  const media = (nota1 + nota2 + nota3) / 3;
+
+  if(media < 5) return `A média do aluno é ${media}, ele está reprovado!`
+  if(media >= 5 && media <= 7) return `A média do aluno é ${media}, ele esta de recuperação!`
+  if(media > 7) return `A média do aluno é ${media}, ele está aprovado!`
+};
+
+console.log(calcularMédia(8, 8, 8));
