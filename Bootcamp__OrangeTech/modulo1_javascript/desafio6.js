11dcd2e8ad6ed46c555c7c2c62c7d48baa754296
Thiago <thiagofang@gmail.com>
2022-10-27 19:47:38 -0300

    bootcamp orangetech: reposta desafio 6

--- Bootcamp__OrangeTech/modulo1_javascript/desafio6.js
+++ Bootcamp__OrangeTech/modulo1_javascript/desafio6.js
@@ -4,3 +4,25 @@
 // As pessoas devem ter a capacidade de dizer o valor do seu IMC ( IMC = peso / (altura * altura));
 // Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao josé
 // para dizer o valor do seu IMC.
+
+class Pessoa {
+  constructor(nome, peso, altura) {
+    this.nome = nome;
+    this.peso = peso;
+    this.altura = altura;
+  };
+
+  calcularIMC() {
+    const IMC = (this.peso / (this.altura * this.altura)).toFixed(2);
+
+    if(IMC < 18.5) return `O IMC de ${this.nome} é ${IMC}, você está abaixo do peso!`;
+    if(IMC >= 18.5 && IMC < 25) return `O IMC de ${this.nome} é ${IMC}, Seu peso esta normal!`;
+    if(IMC >= 25 && IMC < 30) return `O IMC de ${this.nome} é ${IMC}, Você está acima do peso!`;
+    if(IMC >= 30 && IMC < 40) return `O IMC de ${this.nome} é ${IMC}, Você esta com Obesidade!`;
+    if(IMC >=  40) return `O IMC de ${this.nome} é ${IMC}, Você esta com Obesidade grave!`;
+  };
+};
+
+const jose = new Pessoa('José', 78, 1.80);
+
+console.log(jose.calcularIMC());
