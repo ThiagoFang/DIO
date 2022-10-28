b742a61bcc6b92e910a173817d63370466c2aebb
Thiago <thiagofang@gmail.com>
2022-10-28 16:22:36 -0300

    bootcamp orangetech: resposta  desafio 7

--- Bootcamp__OrangeTech/modulo1_javascript/desafio7.js
+++ Bootcamp__OrangeTech/modulo1_javascript/desafio7.js
@@ -1,2 +1,9 @@
 // 7 - Crie um programa que dado um número, imprima a sua tabuada.
 
+const criarTabuada = (numero) => {
+  for(let i = 1; i <= 10; i++) {
+    console.log(i * numero);
+  }
+};
+
+criarTabuada(2)
