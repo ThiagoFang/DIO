a10f783aa8222289215b7967404d965b471ce641
Thiago <thiagofang@gmail.com>
2022-10-28 16:29:37 -0300

    bootcamp orangetech: resposta desafio 8

--- Bootcamp__OrangeTech/modulo1_javascript/desafio8.js
+++ Bootcamp__OrangeTech/modulo1_javascript/desafio8.js
@@ -1,2 +1,14 @@
 // Crie um programa que seja capaz de percorrer uma lista de números e
 // imprima cada número Par encontrado nela.
+
+const listaDeNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const verificarParEmLista = (listaDeNumeros) => {
+  for(let i in listaDeNumeros) {
+    const numero = listaDeNumeros[i];
+
+    if(numero % 2 == 0) console.log(numero);
+  };
+};
+
+verificarParEmLista(listaDeNumeros);
