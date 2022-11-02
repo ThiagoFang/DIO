fefdd36cff5a3455e60feb004fd26b9d62e26456
Thiago <thiagofang@gmail.com>
2022-11-01 22:43:29 -0300

    bootcamp orangetech: adaptando codigo para buscar a informação de mais de um pokemon

--- Bootcamp__OrangeTech/modulo1_javascript/desenvolvimento-web/assets/js/api.js
+++ Bootcamp__OrangeTech/modulo1_javascript/desenvolvimento-web/assets/js/api.js
@@ -1,9 +1,26 @@
 const baseURL = 'https://pokeapi.co/api/v2/';
 
+const convertPokeApiDetailToPokemon = (pokeDetail) => {
+  const pokemon = new Pokemon();
+  pokemon.name = pokeDetail.name;
+  pokemon.id = pokeDetail.id;
+
+  const types = pokeDetail.types.map(typeSlot => typeSlot.type.name);
+  const [type] = types;
+
+  pokemon.types = types;
+  pokemon.type = type;
+
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+  return pokemon;
+}
+
 const apiData = {
   getPokemonDetail: async (pokemon) => {
     return fetch(pokemon.url)
       .then(response => response.json())
+      .then(convertPokeApiDetailToPokemon)
   },
   getPokemonList: async (offset = 0, limit = 3) => {
     const url = `${baseURL}pokemon?offset=${offset}&limit=${limit}`;
--- Bootcamp__OrangeTech/modulo1_javascript/desenvolvimento-web/assets/js/main.js
+++ Bootcamp__OrangeTech/modulo1_javascript/desenvolvimento-web/assets/js/main.js
@@ -1,22 +1,21 @@
 const pokemonAreaHTML = document.querySelector('.pokemons');
 
 const createPokeLi = (pokemon) => {
-  return `<li class="pokemon">
+  return `<li class="pokemon ${pokemon.type}">
   <span class="number">#${pokemon.id}</span>
   <span class="name">${pokemon.name}</span>
 
   <div class="detail">
     <ol class="types">
-      <li class="type">type</li>
-      <li class="type">type</li>
+      ${pokemon.types.map(type => `<li class="type">${type}</li>`).join('')}
     </ol>
 
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg" alt=${pokemon.name}>
+    <img src="${pokemon.photo}" alt=${pokemon.name}>
   </div>
 </li>`;
 };
 
-apiData.getPokemonList().then((pokemons = []) => {
+apiData.getPokemonList(0, 6).then((pokemons = []) => {
   const newHtml = pokemons.map(createPokeLi).join('');
   pokemonAreaHTML.innerHTML = '';
   pokemonAreaHTML.innerHTML += newHtml;
