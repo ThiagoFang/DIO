a7df934fb5e9804d9e42e3aac82af3aedb3d1cf1
Thiago <thiagofang@gmail.com>
2022-11-02 11:27:28 -0300

    desafios de vetores: setup desafio 4

---
+++ desafios-de-codigo/desafio-de-vetores/desafio4/script.js
@@ -0,0 +1,11 @@
+const mainInput = document.querySelector('#mainInput');
+const registerBtn = document.querySelector('.register__button');
+
+registerBtn.addEventListener('click', () => {
+  if(mainInput.value.length < 1) {
+    alert('Preencha o campo de Filme!');
+    return
+  }
+  
+
+});
