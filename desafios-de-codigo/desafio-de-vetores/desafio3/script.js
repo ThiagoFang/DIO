eb6ece1eac0530e02cb625771f86b0a9b70c85ef
Thiago <thiagofang@gmail.com>
2022-11-02 11:33:46 -0300

    desafios de vetores: desafio 3 resposta

--- desafios-de-codigo/desafio-de-vetores/desafio3/script.js
+++ desafios-de-codigo/desafio-de-vetores/desafio3/script.js
@@ -1,11 +1,13 @@
 const mainInput = document.querySelector('#mainInput');
 const registerBtn = document.querySelector('.register__button');
+const moviesList = document.querySelector('.movies__list');
+
+const movies = [];
 
 registerBtn.addEventListener('click', () => {
   if(mainInput.value.length < 1) {
     alert('Preencha o campo de Filme!');
     return
   }
-  
-
+  moviesList.innerHTML += `<li class="movie__item">${mainInput.value}</li>`
 });
