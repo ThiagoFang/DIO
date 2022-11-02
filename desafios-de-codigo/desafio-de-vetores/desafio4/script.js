a8cbc0ad4a2cc039918ec182d82d5fc9243c27d6
Thiago <thiagofang@gmail.com>
2022-11-02 11:49:01 -0300

    desafios de vetores: desafio 4 resposta

--- desafios-de-codigo/desafio-de-vetores/desafio4/script.js
+++ desafios-de-codigo/desafio-de-vetores/desafio4/script.js
@@ -1,11 +1,14 @@
 const mainInput = document.querySelector('#mainInput');
 const registerBtn = document.querySelector('.register__button');
+const moviesList = document.querySelector('.movies__list');
 
+let variant = false;
 registerBtn.addEventListener('click', () => {
   if(mainInput.value.length < 1) {
     alert('Preencha o campo de Filme!');
     return
   }
-  
-
+  const movieClass = variant ? 'movie__item-variant' : 'movie__item';
+  moviesList.innerHTML += `<li class="${movieClass}">${mainInput.value}</li>`
+  variant = !variant
 });
