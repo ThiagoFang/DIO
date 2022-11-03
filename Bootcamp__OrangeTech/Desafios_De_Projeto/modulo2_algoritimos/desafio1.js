1ea16819f6f7b3cf644ef09f641f33b10f2995b0
Thiago <thiagofang@gmail.com>
2022-11-03 18:58:10 -0300

    bootcamp orangetech: Entrando em desafios de algoritimos

---
+++ Bootcamp__OrangeTech/Desafios_De_Projeto/modulo2_algoritimos/desafio1.js
@@ -0,0 +1,8 @@
+// Desafio
+// Dado um número A e o seu limite N, encontre a soma de todos os múltiplos A até o seu limite N.
+
+// Entrada
+// A entrada deverá ser composta pelo valor A na primeira linha, seguido do valor N na segunda. 
+
+// Saída
+// A saída deverá retornar o valor da soma de todos os múltiplos A até o seu limite N.
