ff83822326b4f05ee1acd23ef274a857a904ae1a
Thiago <thiagofang@gmail.com>
2022-11-04 17:08:28 -0300

    bootcamp orangetech: preparando o desafio 3

---
+++ Bootcamp__OrangeTech/modulo2_algoritimos/desafio3.js
@@ -0,0 +1,5 @@
+// Descrição
+// Neste desafio, receba um número inteiro N, calcule e imprima o somatório de todos os números de N até 0.   
+
+// Entrada
+// A Entrada será composta por um número inteiro, N. 
