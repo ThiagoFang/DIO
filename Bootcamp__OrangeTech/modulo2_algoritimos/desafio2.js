67e639059e21a902022294cd94a75863288d5007
Thiago <thiagofang@gmail.com>
2022-11-04 17:05:51 -0300

    bootcamp orangetech: resposta do desafio 2

--- Bootcamp__OrangeTech/modulo2_algoritimos/desafio2.js
+++ Bootcamp__OrangeTech/modulo2_algoritimos/desafio2.js
@@ -7,3 +7,15 @@
 // Se o número for apenas múltiplo de 3 -> "Fizz" ; 
 // Se o número for apenas múltiplo de 5 -> "Buzz"; 
 // Se o número não for um múltiplo de 3 ou 5, o número deve ser exibido;
+
+
+const number = 15;
+
+const fizzBuzz = (number) => {
+  if(number % 3 === 0 && number % 5 === 0) return "FizzBuzz";
+  if(number % 3 === 0) return "Fizz";
+  if(number % 5 === 0) return "Buzz";
+  return number;
+};
+
+console.log(fizzBuzz(number));
