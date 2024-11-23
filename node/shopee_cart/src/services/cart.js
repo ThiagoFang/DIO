78d9dd079dd6bb8f83ad0f8fbb945e4aff650090
Thiago Fang <thiagofang@gmail.com>
2024-11-23 19:07:36 -0300

    mapping cart methods

---
+++ node/shopee_cart/src/services/cart.js
@@ -0,0 +1,15 @@
+async function addItem(userCart) {
+  console.log("addItemToCart");
+}
+
+async function removeItem(userCart, index) {
+  console.log("removeItemFromCart");
+}
+
+async function deleteItem(userCart, name) {
+  console.log("deleteItemFromCart");
+}
+
+async function calculateCartTotal(userCart) {
+  console.log("calculateCartTotal");
+}
---
+++ node/shopee_cart/src/services/item.js
@@ -0,0 +1,3 @@
+export function createItem(name, prince, quantity) {
+
+}
